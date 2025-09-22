ad05b1c413d4ebc2fe0c81b073ca438f0595c2c7
Titus Lim <tituslhy@gmail.com>
2025-09-22 10:12:55 +0800

    shift worker functions to .py file

--- worker_functions.py
+++ worker_functions.py
@@ -1,6 +1,4 @@
 import time
-import asyncio
-from concurrent.futures import ProcessPoolExecutor
 
 def cpu_intensive_work(n):
     """Heavy computation"""
@@ -14,17 +12,3 @@ def io_intensive_work(delay):
     time.sleep(delay)
     return f"Slept for {delay} seconds"
 
-async def async_cpu_test():
-    cpu_tasks = [100, 200, 300, 400]
-    loop = asyncio.get_running_loop()
-    start = time.time()
-    
-    with ProcessPoolExecutor() as executor:
-        results = await asyncio.gather(*[
-            loop.run_in_executor(
-                executor,
-                cpu_intensive_work,
-                task,
-            ) for task in cpu_tasks
-        ])
-    print(f"Asyncio + ProcessPool (CPU): {time.time() - start:.2f}s")
